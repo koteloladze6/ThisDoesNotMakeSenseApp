486cd61eac4f3a7c2e9403a2910ae8e445a7eae9
Kote <k_loladze2@cu.edu.ge>
2020-02-03 14:29:08 +0400

    Add files via upload

---
+++ ThisDoesNotMakeSense/ThisDoesNotMakeSense/MyExtensionMethod/MyExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyExtensionMethod
+{
+    public static class MyExtension
+    {
+        public delegate T MyDelegate<T>(ref IEnumerable<T> list);
+
+        public static T ThisDoesntMakeAnySense<T>(this IEnumerable<T> list, Func<T, bool> predicate, MyDelegate<T> func) where T : new()
+        {
+            if (predicate == null)
+            {
+                throw new Exception("Predicate can not be Null");
+            }
+
+            if (func == null)
+            {
+                throw new Exception("Delegate can not be Null");
+            }
+
+            if (list != null)
+            {
+                foreach (var item in list)
+                {
+
+                    if (predicate(item))
+                    {
+                        return default(T);
+                    }
+
+                }
+            }
+            else
+            {
+                throw new Exception("List can not be Null");
+            }
+
+            return func(ref list);
+        }
+    }
+}
---
+++ ThisDoesNotMakeSense/ThisDoesNotMakeSense/MyExtensionMethod/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string numbersForExample = "1,4,5";
+            List<int> numbers = new List<int>(Array.ConvertAll(numbersForExample.Split(','), int.Parse));
+            int addValue = 8;
+
+            int firstExampleResult = numbers.ThisDoesntMakeAnySense(x => x > 0, (ref IEnumerable<int> data) =>
+            {
+                List<int> temp = new List<int>();
+                temp.AddRange(data);
+                temp.Add(addValue);
+                data = temp;
+                return addValue;
+            });
+
+            Console.WriteLine("First Example Numbers : " + numbersForExample);
+            Console.WriteLine("Predicate : Numbers must be positive ");
+            Console.WriteLine("Output Default Number : " + firstExampleResult + "\n");
+
+            int secondExampleResult = numbers.ThisDoesntMakeAnySense(x => x < 0, (ref IEnumerable<int> data) =>
+            {
+                List<int> temp = new List<int>();
+                temp.AddRange(data);
+                temp.Add(addValue);
+                data = temp;
+                return addValue;
+            });
+
+            Console.WriteLine("Second Example Numbers : " + numbersForExample);
+            Console.WriteLine("Predicate : Numbers must not be positive ");
+            Console.WriteLine("Output newly added  number to the list  : " + secondExampleResult + "\n");
+
+            List<int> emptyList = null;
+
+            Console.WriteLine("Third Example List is Null");
+
+            try
+            {
+                emptyList.ThisDoesntMakeAnySense(x => x > 0, (ref IEnumerable<int> data) =>
+                {
+                    List<int> temp = new List<int>();
+                    temp.AddRange(data);
+                    temp.Add(addValue);
+                    data = temp;
+                    return addValue;
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
